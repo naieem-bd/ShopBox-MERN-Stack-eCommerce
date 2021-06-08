1da6acd71783b4209b06e4509682d1044e56c2ba
naieem-bd <naieemur@gmail.com>
2021-06-08 12:12:43 +0600

    react-router added

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 
 const Header = () => {
@@ -6,15 +7,20 @@ const Header = () => {
         <header>
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">ShopBox</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>ShopBox</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-                            <Nav.Link href="/login"><i className='fas fa-user'></i> Sign In</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link><i className='fas fa-user'></i> Sign In</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
-
                 </Container>
             </Navbar>
         </header>
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <div>
+            Product screen
+        </div>
+    )
+}
+
+export default ProductScreen
