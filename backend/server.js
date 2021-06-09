8e6e186e6c840af190d3693cff1f9d7953e23321
naieem-bd <naieemur@gmail.com>
2021-06-09 11:35:56 +0600

    initial server created

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('API is running...')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
+
+app.listen(7000, console.log('server is running on port 7000'))
