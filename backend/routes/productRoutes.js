efc33e1ab9f34d3abb395125e6bb00ae8dc78413
naieem-bd <naieemur@gmail.com>
2021-06-17 10:50:41 +0600

    add error middleware

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -16,10 +16,12 @@ router.get('/', asyncHandler(async (req, res) => {
 // @access  Public
 router.get('/:id', asyncHandler( async (req, res) => {
     const product = await Product.findById(req.params.id)
+
     if(product) {
         res.json(product)
     } else {
-        res.status(404).json({ message: 'Product not found' })
+        res.status(404)
+        throw new Error('Product not found')
     }
 }))
 
