0a892a2daefe1bb478f77cda7fc5cc563a688a6c
naieem-bd <naieemur@gmail.com>
2023-05-29 13:51:51 +0600

    product controller added

---
+++ backend/controllers/productController.js
@@ -0,0 +1,22 @@
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+// Fetch all products
+const getProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+  res.json(products);
+});
+
+// Fetch single product
+const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error('Product not found');
+  }
+});
+
+export { getProducts, getProductById };
