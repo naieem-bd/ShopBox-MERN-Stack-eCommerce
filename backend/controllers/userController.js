181170339ad400b6fe8a6c4c3b8f213edb220c78
naieem-bd <naieemur@gmail.com>
2023-05-31 16:46:50 +0600

    auth middleware added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,4 +22,21 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser };
+// get user profile
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export { authUser, getUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { authUser } from '../controllers/userController.js';
+import { authUser, getUserProfile } from '../controllers/userController.js';
+import { protect } from '../middleware/authMiddleware.js';
 
 // user login
 router.post('/login', authUser);
+
+// get a user profile
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
