90de0b4240587d6322b044fdef6bd7e573333a21
naieem-bd <naieemur@gmail.com>
2023-05-31 11:19:10 +0600

    jwt added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utills/generateToken.js';
 import User from '../models/userModel.js';
 
 // Fetch all products
@@ -13,7 +14,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null,
+      token: generateToken(user._id),
     });
   } else {
     res.status(401);
