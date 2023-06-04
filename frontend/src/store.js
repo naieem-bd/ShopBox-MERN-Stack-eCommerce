1e940f736e5bb3bd0493cee1a2234b1eaf69c357
naieem-bd <naieemur@gmail.com>
2023-06-04 12:46:44 +0600

    user action added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -19,8 +19,13 @@ const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
   : [];
 
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const middleware = [thunk];
