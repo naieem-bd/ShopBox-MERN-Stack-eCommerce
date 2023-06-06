25b0424f8c1de647564a62a60217de6cff3cd7e3
naieem-bd <naieemur@gmail.com>
2023-06-06 15:22:57 +0600

    user profile update

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -9,6 +9,9 @@ import {
   USER_DETAILS_REQUEST,
   USER_DETAILS_SUCCESS,
   USER_DETAILS_FAIL,
+  USER_PROFILE_UPDATE_REQUEST,
+  USER_PROFILE_UPDATE_SUCCESS,
+  USER_PROFILE_UPDATE_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -49,8 +52,19 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
       return { loading: false, user: action.payload };
     case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
-    case USER_LOGOUT:
-      return {};
+    default:
+      return state;
+  }
+};
+
+export const userProfileUpdateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_PROFILE_UPDATE_REQUEST:
+      return { loading: true };
+    case USER_PROFILE_UPDATE_SUCCESS:
+      return { loading: false, success: true, userInfo: action.payload };
+    case USER_PROFILE_UPDATE_FAIL:
+      return { loading: false, error: action.payload };
     default:
       return state;
   }
