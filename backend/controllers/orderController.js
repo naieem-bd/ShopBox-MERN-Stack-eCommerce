1a7a34a1f0aa7758e9048f4928657050400ea580
naieem-bd <naieemur@gmail.com>
2023-07-05 16:22:36 +0600

    order action, reducer, screen etc

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,112 @@
+import asyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js';
+
+// Create new order
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400);
+    throw new Error('No order items');
+    return;
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      taxPrice,
+      shippingPrice,
+      totalPrice,
+    });
+
+    const createdOrder = await order.save();
+
+    res.status(201).json(createdOrder);
+  }
+});
+
+// Get order by ID
+// const getOrderById = asyncHandler(async (req, res) => {
+//   const order = await Order.findById(req.params.id).populate(
+//     'user',
+//     'name email'
+//   )
+
+//   if (order) {
+//     res.json(order)
+//   } else {
+//     res.status(404)
+//     throw new Error('Order not found')
+//   }
+// })
+
+// Update order to paid
+// const updateOrderToPaid = asyncHandler(async (req, res) => {
+//   const order = await Order.findById(req.params.id)
+
+//   if (order) {
+//     order.isPaid = true
+//     order.paidAt = Date.now()
+//     order.paymentResult = {
+//       id: req.body.id,
+//       status: req.body.status,
+//       update_time: req.body.update_time,
+//       email_address: req.body.payer.email_address,
+//     }
+
+//     const updatedOrder = await order.save()
+
+//     res.json(updatedOrder)
+//   } else {
+//     res.status(404)
+//     throw new Error('Order not found')
+//   }
+// })
+
+// Update order to delivered
+// const updateOrderToDelivered = asyncHandler(async (req, res) => {
+//   const order = await Order.findById(req.params.id)
+
+//   if (order) {
+//     order.isDelivered = true
+//     order.deliveredAt = Date.now()
+
+//     const updatedOrder = await order.save()
+
+//     res.json(updatedOrder)
+//   } else {
+//     res.status(404)
+//     throw new Error('Order not found')
+//   }
+// })
+
+// Get logged in user orders
+// const getMyOrders = asyncHandler(async (req, res) => {
+//   const orders = await Order.find({ user: req.user._id })
+//   res.json(orders)
+// })
+
+// Get all orders
+// const getOrders = asyncHandler(async (req, res) => {
+//   const orders = await Order.find({}).populate('user', 'id name')
+//   res.json(orders)
+// })
+
+export {
+  addOrderItems,
+  // getOrderById,
+  // updateOrderToPaid,
+  // updateOrderToDelivered,
+  // getMyOrders,
+  // getOrders,
+};
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/login' component={LoginScreen} />
