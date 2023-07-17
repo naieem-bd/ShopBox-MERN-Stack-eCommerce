aa25d674800270fe952e84cb002e809b211e147a
naieem-bd <naieemur@gmail.com>
2023-07-17 13:09:58 +0600

    product create and update

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,13 +4,19 @@ import {
   getProducts,
   getProductById,
   deleteProduct,
+  createProduct,
+  updateProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 // Fetch all products
-router.route('/').get(getProducts);
+router.route('/').get(getProducts).post(protect, admin, createProduct);
 
 // Fetch single product
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
 
 export default router;
