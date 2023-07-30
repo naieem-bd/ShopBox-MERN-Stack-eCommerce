3cb331be1c01271377788a793f98e132e208d7ee
naieem-bd <naieemur@gmail.com>
2023-07-30 20:40:39 +0600

    product details screen ui modify

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,11 +9,13 @@ import {
   Card,
   Button,
   Form,
+  Badge,
 } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Message from '../components/Message';
 import Loader from '../components/Loader';
 import Meta from '../components/Meta';
+import ImgDron from '../images/dron.jpg';
 import {
   listProductDetails,
   createProductReview,
@@ -66,7 +68,7 @@ const ProductScreen = ({ history, match }) => {
   };
 
   return (
-    <>
+    <div className='product-details'>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
@@ -78,10 +80,70 @@ const ProductScreen = ({ history, match }) => {
         <>
           <Meta title={product.name} />
           <Row>
-            <Col md={6}>
-              <Image src={product.image} alt={product.name} fluid />
+            <Col md={6} className='mb-4'>
+              <Image
+                className='boxShadow'
+                src={product.image}
+                alt={product.name}
+                fluid
+              />
             </Col>
-            <Col md={3}>
+            <Col md={6} className='mb-4'>
+              <div className='pro-dtl-box'>
+                <h3>{product.name}</h3>
+                <p>
+                  <span>Brand : </span> {product.brand}
+                </p>
+                <p>
+                  <span>Category : </span> {product.category}
+                </p>
+                <p>
+                  <span>Abailability : </span>
+                  {product.countInStock > 0 ? (
+                    <span style={{ color: 'green' }}>In Stock</span>
+                  ) : (
+                    <span style={{ color: 'red' }}>Out Of Stock</span>
+                  )}
+                </p>
+                <div className='mt-3'>
+                  <Rating
+                    value={product.rating}
+                    text={`(${product.numReviews} Reviews)`}
+                  />
+                </div>
+                <h3>
+                  Price: <strong>${product.price}</strong>
+                </h3>
+                <div className='d-flex'>
+                  <Form.Control
+                    className='mr-2'
+                    as='select'
+                    style={{ width: 70 }}
+                    value={qty}
+                    onChange={(e) => setQty(e.target.value)}>
+                    {[...Array(product.countInStock).keys()].map((x) => (
+                      <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </option>
+                    ))}
+                  </Form.Control>
+                  <Button
+                    onClick={addToCartHandler}
+                    type='button'
+                    disabled={product.countInStock === 0}>
+                    <i class='fas fa-shopping-basket mr-2'></i>
+                    Add To Cart
+                  </Button>
+                </div>
+                {/* <hr /> */}
+                <p className='mt-4'>
+                  <span>Description : </span>
+                  {product.description}
+                </p>
+              </div>
+            </Col>
+
+            {/* <Col md={6}>
               <ListGroup variant='flush'>
                 <ListGroup.Item>
                   <h3>{product.name}</h3>
@@ -98,7 +160,7 @@ const ProductScreen = ({ history, match }) => {
                 </ListGroup.Item>
               </ListGroup>
             </Col>
-            <Col md={3}>
+            <Col md={6}>
               <Card>
                 <ListGroup variant='flush'>
                   <ListGroup.Item>
@@ -152,11 +214,14 @@ const ProductScreen = ({ history, match }) => {
                   </ListGroup.Item>
                 </ListGroup>
               </Card>
-            </Col>
+            </Col> */}
           </Row>
+
+          <hr className='mb-5' />
+
           <Row>
-            <Col md={6}>
-              <h2>Reviews</h2>
+            <Col md={7}>
+              <h2 class='sub-heading'>Customers Reviews</h2>
               {product.reviews.length === 0 && <Message>No Reviews</Message>}
               <ListGroup variant='flush'>
                 {product.reviews.map((review) => (
@@ -219,140 +284,43 @@ const ProductScreen = ({ history, match }) => {
                 </ListGroup.Item>
               </ListGroup>
             </Col>
+            <Col md={5}>
+              <h2 class='sub-heading'>Top Products</h2>
+
+              <Card className='box mb-4'>
+                <Badge pill variant='warning' className='text-white'>
+                  Top Rated
+                </Badge>
+                <Row>
+                  <Col xs={5}>
+                    <Image src={ImgDron} fluid />
+                  </Col>
+                  <Col xs={7}>
+                    <div className='product-info'>
+                      <p className='pro-name mt-2 line-clamp-2'>
+                        <Link to='#'>
+                          This is the very very lo...ng long product name will
+                          goes here
+                        </Link>
+                      </p>
+                      <div className='pro-rivew my-2'>
+                        <Rating value={4} text='7 Reviews' />
+                      </div>
+                      <p className='pro-price mb-3'>Price: $527</p>
+                      <Button variant='outline-primary' size='sm'>
+                        <i class='fas fa-plus mr-2'></i>
+                        Add To Cart
+                      </Button>
+                    </div>
+                  </Col>
+                </Row>
+              </Card>
+            </Col>
           </Row>
         </>
       )}
-    </>
+    </div>
   );
 };
 
 export default ProductScreen;
-
-// import React, { useState, useEffect } from 'react';
-// import { Link } from 'react-router-dom';
-// import { useDispatch, useSelector } from 'react-redux';
-// import {
-//   Row,
-//   Col,
-//   Image,
-//   ListGroup,
-//   Card,
-//   Button,
-//   Form,
-// } from 'react-bootstrap';
-// import Rating from '../components/Rating';
-// import Message from '../components/Message';
-// import Loader from '../components/Loader';
-// import {
-//   listProductDetails,
-//   createProductReview,
-// } from '../actions/productActions';
-
-// const ProductScreen = ({ history, match }) => {
-//   const [qty, setQty] = useState(1);
-
-//   const dispatch = useDispatch();
-
-//   const productDetails = useSelector((state) => state.productDetails);
-//   const { loading, error, product } = productDetails;
-
-//   useEffect(() => {
-//     dispatch(listProductDetails(match.params.id));
-//   }, [dispatch, match]);
-
-//   const addToCartHandler = () => {
-//     history.push(`/cart/${match.params.id}?qty=${qty}`);
-//   };
-
-//   return (
-//     <>
-//       <Link className='btn btn-light my-3' to='/'>
-//         Go Back
-//       </Link>
-//       {loading ? (
-//         <Loader />
-//       ) : error ? (
-//         <Message variant='danger'>{error}</Message>
-//       ) : (
-//         <Row>
-//           <Col md={6}>
-//             <Image src={product.image} alt={product.name} fluid />
-//           </Col>
-//           <Col md={3}>
-//             <ListGroup variant='flush'>
-//               <ListGroup.Item>
-//                 <h3>{product.name}</h3>
-//               </ListGroup.Item>
-//               <ListGroup.Item>
-//                 <Rating
-//                   value={product.rating}
-//                   text={`${product.numReviews} reviews`}
-//                 />
-//               </ListGroup.Item>
-//               <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
-//               <ListGroup.Item>
-//                 Description: {product.description}
-//               </ListGroup.Item>
-//             </ListGroup>
-//           </Col>
-//           <Col md={3}>
-//             <Card>
-//               <ListGroup variant='flush'>
-//                 <ListGroup.Item>
-//                   <Row>
-//                     <Col>Price:</Col>
-//                     <Col>
-//                       <strong>${product.price}</strong>
-//                     </Col>
-//                   </Row>
-//                 </ListGroup.Item>
-
-//                 <ListGroup.Item>
-//                   <Row>
-//                     <Col>Status:</Col>
-//                     <Col>
-//                       {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
-//                     </Col>
-//                   </Row>
-//                 </ListGroup.Item>
-
-//                 {product.countInStock > 0 && (
-//                   <ListGroup.Item>
-//                     <Row className='align-items-center'>
-//                       <Col>Qty:</Col>
-//                       <Col>
-//                         <Form.Control
-//                           as='select'
-//                           size='sm'
-//                           value={qty}
-//                           onChange={(e) => setQty(e.target.value)}>
-//                           {[...Array(product.countInStock).keys()].map((x) => (
-//                             <option key={x + 1} value={x + 1}>
-//                               {x + 1}
-//                             </option>
-//                           ))}
-//                         </Form.Control>
-//                       </Col>
-//                     </Row>
-//                   </ListGroup.Item>
-//                 )}
-
-//                 <ListGroup.Item>
-//                   <Button
-//                     onClick={addToCartHandler}
-//                     className='btn-block'
-//                     type='button'
-//                     disabled={product.countInStock === 0}>
-//                     Add To Cart
-//                   </Button>
-//                 </ListGroup.Item>
-//               </ListGroup>
-//             </Card>
-//           </Col>
-//         </Row>
-//       )}
-//     </>
-//   );
-// };
-
-// export default ProductScreen;
