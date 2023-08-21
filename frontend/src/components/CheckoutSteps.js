5219b8e591cb5209b28927f2a77e7f1dd04f4a6b
naieem-bd <naieemur@gmail.com>
2023-08-21 17:45:00 +0600

    ui modification

--- frontend/src/components/CheckoutSteps.js
+++ frontend/src/components/CheckoutSteps.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Nav } from 'react-bootstrap';
+import { Card, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
@@ -8,40 +8,64 @@ const CheckoutSteps = ({ step1, step2, step3, step4 }) => {
       <Nav.Item>
         {step1 ? (
           <LinkContainer to='/login'>
-            <Nav.Link>Sign In</Nav.Link>
+            <Nav.Link className='btn btn-primary btn-sm px-2 py-1 mx-1'>
+              Sign In
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Sign In</Nav.Link>
+          <Nav.Link
+            disabled
+            className='btn btn-secondary btn-sm px-2 py-1 mx-1'>
+            Sign In
+          </Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step2 ? (
           <LinkContainer to='/shipping'>
-            <Nav.Link>Shipping</Nav.Link>
+            <Nav.Link className='btn btn-primary btn-sm px-2 py-1 mx-1'>
+              Shipping
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Shipping</Nav.Link>
+          <Nav.Link
+            disabled
+            className='btn btn-secondary btn-sm px-2 py-1 mx-1'>
+            Shipping
+          </Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step3 ? (
           <LinkContainer to='/payment'>
-            <Nav.Link>Payment</Nav.Link>
+            <Nav.Link className='btn btn-primary btn-sm px-2 py-1 mx-1'>
+              Payment
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Payment</Nav.Link>
+          <Nav.Link
+            disabled
+            className='btn btn-secondary btn-sm px-2 py-1 mx-1'>
+            Payment
+          </Nav.Link>
         )}
       </Nav.Item>
 
       <Nav.Item>
         {step4 ? (
           <LinkContainer to='/placeorder'>
-            <Nav.Link>Place Order</Nav.Link>
+            <Nav.Link className='btn btn-primary btn-sm px-2 py-1 mx-1'>
+              Place Order
+            </Nav.Link>
           </LinkContainer>
         ) : (
-          <Nav.Link disabled>Place Order</Nav.Link>
+          <Nav.Link
+            disabled
+            className='btn btn-secondary btn-sm px-2 py-1 mx-1'>
+            Place Order
+          </Nav.Link>
         )}
       </Nav.Item>
     </Nav>
