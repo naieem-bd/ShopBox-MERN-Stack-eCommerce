bbd483251b517340d0eaadb616fda41f4b20c6bd
naieem-bd <naieemur@gmail.com>
2023-08-27 17:10:54 +0600

    new product added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -17,6 +17,7 @@ const getProducts = asyncHandler(async (req, res) => {
 
   const count = await Product.countDocuments({ ...keyword });
   const products = await Product.find({ ...keyword })
+    .sort({ createdAt: -1 })
     .limit(pageSize)
     .skip(pageSize * (page - 1));
 
