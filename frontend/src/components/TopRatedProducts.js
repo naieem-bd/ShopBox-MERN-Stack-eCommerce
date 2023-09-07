4cb96c853b94e3457474427d12a88d34fb3fd852
naieem-bd <naieemur@gmail.com>
2023-09-07 14:19:29 +0600

    product fetch from server

--- frontend/src/components/TopRatedProducts.js
+++ frontend/src/components/TopRatedProducts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Row, Col, Card, Image, Badge, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import ImgTablate from '../images/tablate.jpg';
@@ -7,8 +7,27 @@ import ImgDron from '../images/dron.jpg';
 import ImgAirphone from '../images/airphone.jpg';
 import Rating from './Rating';
 
+import { useDispatch, useSelector } from 'react-redux';
+import Loader from './Loader';
+import Message from './Message';
+import { listTopProducts } from '../actions/productActions';
+
 const TopRatedProducts = () => {
-  return (
+  const dispatch = useDispatch();
+
+  const productTopRated = useSelector((state) => state.productTopRated);
+  const { loading, error, products } = productTopRated;
+  console.log(products);
+
+  useEffect(() => {
+    dispatch(listTopProducts());
+  }, [dispatch]);
+
+  return loading ? (
+    <Loader />
+  ) : error ? (
+    <Message variant='danger'>{error}</Message>
+  ) : (
     <>
       <Row>
         <Col lg={7}>
@@ -18,29 +37,36 @@ const TopRatedProducts = () => {
             </Badge>
             <Row>
               <Col xs={7}>
-                <Image
-                  src={ImgTablate}
-                  fluid
-                  className='my-1'
-                  style={{ paddingTop: 2 }}
-                />
+                <Link to={`/product/${products[0]?._id}`}>
+                  <Image
+                    src={products[0]?.image}
+                    fluid
+                    className='my-1'
+                    style={{ paddingTop: 2 }}
+                  />
+                </Link>
               </Col>
               <Col xs={5}>
                 <div className='product-info mb-4 pt-4 pb-3'>
                   <p className='pro-name mt-4 line-clamp-2'>
-                    <Link to='#'>
-                      This is the very long long product name will goes here
+                    <Link to={`/product/${products[0]?._id}`}>
+                      {products[0]?.name}
                     </Link>
                   </p>
                   <div className='pro-rivew my-3'>
-                    <Rating value={4} text='7 reviews' />
+                    <Rating
+                      value={products[0]?.rating}
+                      text={`(${products[0]?.numReviews} Reviews)`}
+                    />
                   </div>
-                  <p className='pro-price'>Price: $527</p>
+                  <p className='pro-price'>Price: ${products[0]?.price}</p>
                 </div>
-                <Button variant='outline-primary'>
-                  <i class='fas fa-plus mr-2'></i>
-                  Add To Cart
-                </Button>
+                <Link to={`/product/${products[0]?._id}`}>
+                  <Button variant='outline-primary'>
+                    <i class='fas fa-plus mr-2'></i>
+                    Add To Cart
+                  </Button>
+                </Link>
               </Col>
             </Row>
           </Card>
@@ -52,24 +78,30 @@ const TopRatedProducts = () => {
             </Badge>
             <Row>
               <Col xs={5}>
-                <Image src={ImgDron} fluid />
+                <Link to={`/product/${products[1]?._id}`}>
+                  <Image src={products[1]?.image} fluid />
+                </Link>
               </Col>
               <Col xs={7}>
                 <div className='product-info'>
                   <p className='pro-name mt-2 line-clamp-2'>
-                    <Link to='#'>
-                      This is the very very lo...ng long product name will goes
-                      here
+                    <Link to={`/product/${products[1]?._id}`}>
+                      {products[1]?.name}
                     </Link>
                   </p>
                   <div className='pro-rivew my-2'>
-                    <Rating value={4} text='7 Reviews' />
+                    <Rating
+                      value={products[1]?.rating}
+                      text={`(${products[1]?.numReviews} Reviews)`}
+                    />
                   </div>
-                  <p className='pro-price mb-3'>Price: $527</p>
-                  <Button variant='outline-primary' size='sm'>
-                    <i class='fas fa-plus mr-2'></i>
-                    Add To Cart
-                  </Button>
+                  <p className='pro-price mb-3'>Price: ${products[1]?.price}</p>
+                  <Link to={`/product/${products[1]?._id}`}>
+                    <Button variant='outline-primary' size='sm'>
+                      <i class='fas fa-plus mr-2'></i>
+                      Add To Cart
+                    </Button>
+                  </Link>
                 </div>
               </Col>
             </Row>
@@ -80,24 +112,30 @@ const TopRatedProducts = () => {
             </Badge>
             <Row>
               <Col xs={5}>
-                <Image src={ImgHeadphone} fluid />
+                <Link to={`/product/${products[2]?._id}`}>
+                  <Image src={products[2]?.image} fluid />
+                </Link>
               </Col>
               <Col xs={7}>
                 <div className='product-info'>
                   <p className='pro-name mt-2 line-clamp-2'>
-                    <Link to='#'>
-                      This is the very very lo...ng long product name will goes
-                      here
+                    <Link to={`/product/${products[2]?._id}`}>
+                      {products[2]?.name}
                     </Link>
                   </p>
                   <div className='pro-rivew my-2'>
-                    <Rating value={4} text='7 Reviews' />
+                    <Rating
+                      value={products[2]?.rating}
+                      text={`(${products[2]?.numReviews} Reviews)`}
+                    />
                   </div>
-                  <p className='pro-price mb-3'>Price: $527</p>
-                  <Button variant='outline-primary' size='sm'>
-                    <i class='fas fa-plus mr-2'></i>
-                    Add To Cart
-                  </Button>
+                  <p className='pro-price mb-3'>Price: ${products[2]?.price}</p>
+                  <Link to={`/product/${products[2]?._id}`}>
+                    <Button variant='outline-primary' size='sm'>
+                      <i class='fas fa-plus mr-2'></i>
+                      Add To Cart
+                    </Button>
+                  </Link>
                 </div>
               </Col>
             </Row>
