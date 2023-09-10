2ae425fd9dedb4929fbef14a8def8ea65449ceff
naieem-bd <naieemur@gmail.com>
2023-09-10 12:13:14 +0600

    ui modification

--- frontend/src/components/FormContainer.js
+++ frontend/src/components/FormContainer.js
@@ -3,13 +3,11 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const FormContainer = ({ children }) => {
   return (
-    <Container>
-      <Row className='justify-content-md-center'>
-        <Col xs={12} md={6}>
-          {children}
-        </Col>
-      </Row>
-    </Container>
+    <Row className='justify-content-md-center align-items-center logRegForm'>
+      <Col xs={12} md={6}>
+        {children}
+      </Col>
+    </Row>
   );
 };
 
